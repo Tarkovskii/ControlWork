401f3e095f5027b9c817ad5d80124c25a27d4df4
Daniil <trombonil1996@gmail.com>
2022-09-16 18:05:45 +0300

    DoneCod

---
+++ Code/Program.cs
@@ -0,0 +1,74 @@
+﻿Console.WriteLine("Сколько элементов будет в массиве?");
+bool countElements = int.TryParse(Console.ReadLine(), out int count);
+
+if (countElements == false)
+{
+    Console.WriteLine("Введены некорректные данные!");
+}
+
+string[] FillArray(int count)
+{
+    string[] arr = new string[count];
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine("Введите что-нибудь");
+        string element = Convert.ToString(Console.ReadLine());
+        arr[i] = element;
+    }
+    return arr;
+}
+
+int GetCountElementsThreeSymbols(string[] arr)
+{
+    count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string checkElement = arr[i];
+        int simbolsElement = checkElement.Length;
+
+        if (simbolsElement <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+
+}
+
+string[] CreateArrFromeElementsTillThreeSimbols(string[] arr, int count)
+{
+    int index = 0;
+
+    string[] resArr = new string[count];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string checkElement = arr[i];
+        int simbolsElement = checkElement.Length;
+
+        if (simbolsElement <= 3)
+        {
+            resArr[index] = arr[i];
+            index++;
+        }
+    }
+    return resArr;
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+Console.WriteLine();
+string[] arrElements = FillArray(count);
+int sizeResArr = GetCountElementsThreeSymbols(arrElements);
+string[] resArr = CreateArrFromeElementsTillThreeSimbols(arrElements, sizeResArr);
+PrintArray(arrElements);
+Console.WriteLine();
+PrintArray(resArr);
+
